e36b772758e6dd3c9954ea61ff21189c694338fd
naoto <naoto.oct25.g@gmail.com>
2019-10-07 23:29:11 +0900

    add:ABC131 A-C追加
    C問題むずめ。LCMとGCD

---
+++ src/main/bignner/contest131/A_Security.java
@@ -0,0 +1,21 @@
+package main.bignner.contest131;
+
+import java.util.Scanner;
+
+public class A_Security {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        char[] c = s.toCharArray();
+        String ans = "Good";
+        for (int i = 1; i < c.length; i++) {
+            if (c[i] == c[i - 1]) {
+                ans = "Bad";
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
---
+++ src/main/bignner/contest131/B_BiteEating.java
@@ -0,0 +1,31 @@
+package main.bignner.contest131;
+
+import java.util.Scanner;
+
+public class B_BiteEating {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int l = sc.nextInt();
+
+        int[] taste = new int[n];
+        for (int i = 1; i <= n; i++) {
+            taste[i - 1] = l + i - 1;
+        }
+
+        int min = 105;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            min = Math.min(Math.abs(taste[i]), min);
+            sum += taste[i];
+        }
+
+        if (sum > 0) {
+            System.out.println(sum - min);
+        } else {
+            // 0の場合は均等なので最小値も0で加算しても変わらない
+            System.out.println(sum + min);
+        }
+    }
+}
