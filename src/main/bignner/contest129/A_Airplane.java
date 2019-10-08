f40d14bd97a38be76efb7c949a86bea72415977a
naoto <naoto.oct25.g@gmail.com>
2019-10-08 18:54:30 +0900

    add:ABC129 A-C追加
    C:DPというよりは累積和

---
+++ src/main/bignner/contest129/A_Airplane.java
@@ -0,0 +1,24 @@
+package main.bignner.contest129;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class A_Airplane {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();
+        int q = sc.nextInt();
+        int r = sc.nextInt();
+
+        int[] route = { p, q, r };
+        Arrays.sort(route);
+
+        int ans = 0;
+        ans += route[0];
+        ans += route[1];
+
+        System.out.println(ans);
+    }
+
+}
---
+++ src/main/bignner/contest129/B_Balance.java
@@ -0,0 +1,34 @@
+package main.bignner.contest129;
+
+import java.util.Scanner;
+
+public class B_Balance {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] w = new int[n];
+        for (int i = 0; i < n; i++) {
+            w[i] = sc.nextInt();
+        }
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += w[i];
+        }
+
+        int ans = sum;
+        for (int i = 1; i <= n; i++) {
+            int under = 0;
+            for (int j = 0; j < i; j++) {
+                under += w[j];
+            }
+            // System.out.printf("%s -> %s : %s = %s\n", i, under, sum - under,
+            // Math.abs(sum - under - under));
+            ans = Math.min(Math.abs(sum - under - under), ans);
+        }
+
+        System.out.println(ans);
+    }
+
+}
